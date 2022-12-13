1a5d6db02c9a6fc906f5bd462e0ab752ebb78e57
Ch1nmayH <cod1.1backup@gmail.com>
2022-12-14 00:03:59 +0530

    nodemailer method to send verification link after signup

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,7 @@
 const User = require("../model/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { transporter } = require("../controllers/nodemailerConnect");
 
 const handleError = (error) => {
   let errors = {};
@@ -12,19 +14,62 @@ const handleError = (error) => {
   return errors;
 };
 
+const handleVerification = async ({ _id, email }, res) => {
+  transporter.verify((error, result) => {
+    let a = 100;
+    if (error) {
+      console.log(error);
+    }
+
+    if (result) {
+      console.log("ready to send message");
+      console.log(result);
+    }
+  });
+
+  let currentUrl = process.env.CURRENTURL;
+
+  let idToken = await jwt.sign({ id: _id.toString() }, process.env.SECRET, {
+    expiresIn: 60 * 60,
+  });
+
+  let verifyUrl = currentUrl + "/" + idToken;
+  let mailOptions = {
+    from: process.env.AUTH_EMAIL,
+    to: email,
+    subject: "Email Verification",
+    html: `<p>Click on this <a href='http://localhost:8000/api/verify/${idToken}'> link </a> to verify your email </p>`,
+  };
+  transporter
+    .sendMail(mailOptions)
+    .then(() => {
+      console.log("email sent");
+    })
+    .catch((e) => {
+      console.log(e.message);
+    });
+
+  // return verifyUrl;
+};
+
 module.exports.signup_post = async (req, res, next) => {
   let { name, email, password } = req.body;
   try {
     let exist = await User.findOne({ email });
 
     if (exist) {
+      console.log(process.env.CURRENTURL);
       return res.status(406).send("Email already exists");
     }
 
     let hashedPassword = await bcrypt.hash(password, 10);
     console.log(hashedPassword);
     let user = await User.create({ name, email, password: hashedPassword });
-    res.status(201).send(user);
+    handleVerification(user, res);
+    // let verifyUrl = await handleVerification(user, res);
+    // console.log(verifyUrl);
+    // res.send(verifyUrl);
+    // res.status(201).send(user);
     next();
   } catch (error) {
     let errorMessage = handleError(error);
@@ -43,13 +88,18 @@ module.exports.login_post = async (req, res, next) => {
       let comparePassword = await bcrypt.compare(password, user.password);
       if (comparePassword) {
         // res.redirect("/");
-        return res.send(`${user.name}, you have successfully LoggedIn`);
+        if (user.verified) {
+          return res.send(`${user.name}, you have successfully LoggedIn`);
+        } else {
+          return res.send(`${user.name}, you need to verify your email`);
+        }
       }
+
       return res.send("Wrong Password");
     }
     return res.send("Wrong Email id");
 
-    res.status(201).send("Login here");
+    // res.status(201).send("Login here");
     next();
   } catch (error) {
     res.send(error.message);
