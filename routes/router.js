19aa9a23ab829cc42d4d701daeb4ecfa5ab691f9
Ch1nmayH <cod1.1backup@gmail.com>
2022-12-14 15:03:56 +0530

    added routes to reset the user password

--- routes/router.js
+++ routes/router.js
@@ -4,4 +4,9 @@ const controller = require("../controllers/controller");
 router.post("/signup", controller.signup_post);
 router.post("/login", controller.login_post);
 
+router.post("/forgotPassword", controller.forgotPassword_post);
+router.get("/forgotPassword/:id", controller.newPassword);
+
+router.post("/resetPassword/:id", controller.resetPassword);
+
 module.exports = router;
