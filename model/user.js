cad4557ee7aa51d582657e8cc31835d1aaeaebd9
Ch1nmayH <cod1.1backup@gmail.com>
2022-12-28 17:35:57 +0530

    New error messages and email validation

--- model/user.js
+++ model/user.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
+const isEmail = require("validator/lib/isEmail");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  name: { type: String, required: [true, "Please enter your name"] },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+    validate: [isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    min: 6,
+    required: [true, "Please Enter the password"],
+  },
   verified: { type: Boolean, default: false },
 });
 
