e8195cc23f44eb9516f6441edcb867806c6ead6d
Ch1nmayH <cod1.1backup@gmail.com>
2022-12-29 17:05:49 +0530

    schema pre function to hash password and custom statics method to compare the password

--- model/user.js
+++ model/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const isEmail = require("validator/lib/isEmail");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: [true, "Please enter your name"] },
@@ -11,10 +12,36 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    min: 6,
+    minLength: [6, "Password must be atleast 6 characters long"],
     required: [true, "Please Enter the password"],
   },
-  verified: { type: Boolean, default: false },
+  verified: { type: Boolean, default: true },
+  role: { type: String, default: "member" },
 });
+
+userSchema.pre("save", async function (next) {
+  try {
+    let salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    throw Error("Unable to create the user");
+    next();
+  }
+});
+
+userSchema.statics.login = async function (email, password) {
+  let user = await this.findOne({ email });
+  if (user) {
+    let comparePassword = await bcrypt.compare(password, user.password);
+    if (comparePassword) {
+      return user;
+    }
+    throw Error("Invalid Password");
+  }
+  throw Error("Invalid Email Id");
+};
 
 module.exports = mongoose.model("user", userSchema);
